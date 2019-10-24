e56588bd2b8f2abdea3a30930e08aca779e8574d
Jorge Luis Vásquez <jorge.vasquez@utec.edu.pe>
2019-10-23 22:41:24 -0500

    Primer Commit

---
+++ cmake-build-debug/CMakeFiles/TranbajoFinal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TranbajoFinal.dir/main.cpp.obj"
+  "TranbajoFinal.pdb"
+  "TranbajoFinal.exe"
+  "TranbajoFinal.exe.manifest"
+  "libTranbajoFinal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TranbajoFinal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
